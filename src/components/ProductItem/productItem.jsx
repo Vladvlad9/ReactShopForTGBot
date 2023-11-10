34b7bc5188f8fd5bbb70ed96f16fc351f9068d9a
Vlad Paulechka <vlad9.ru@gmail.com>
2023-11-10 13:09:36 +0300

    update

--- src/components/ProductItem/productItem.jsx
+++ src/components/ProductItem/productItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from "../Button/Button";
-import './ProductItem.css';
+import './productItem.css';
 
 const ProductItem = ({product, className, onAdd}) => {
 
